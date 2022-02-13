1d4a27d4fe61e15e84d6de698766c11be63df5e1
Natchaya325 <snattncy@gmail.com>
2022-02-13 17:55:53 +0700

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Shop from '../views/Shop.vue'
+import Detail from '../components/Detail.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,17 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: "/shop",
+    component: Shop,
+    children: [
+      {
+        path: "/detail/:id",
+        name: "detail",
+        component: Detail,
+      },
+    ],
   },
   {
     path: '/about',
